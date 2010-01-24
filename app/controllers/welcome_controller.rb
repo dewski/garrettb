e0eb67b412befe471d1ee45a03a18160504bab25
dewski <xhtmlthis@me.com>
2010-01-24 02:51:39 -0800

    fixed welcome page thumbnail issues

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,6 +1,6 @@
 class WelcomeController < ApplicationController
   def index
-    @items = Item.where("images_count > 0").limit(9)
+    @items = Item.where("images_count > 0").order("name ASC").limit(9)
     @posts = Post.where("published = 1").order("published_at DESC").limit(3)
   end
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -10,7 +10,7 @@ class Item < ActiveRecord::Base
   validates_format_of :url, :with => /^(http|https):\/\/[a-z0-9]+([\-\.]{1}[a-z0-9]+)*\.[a-z]{2,5}(([0-9]{1,5})?\/.*)?$/ix
   
   def thumbnail
-    images.order('position DESC').first
+    images.order('position ASC').first
   end
   
   def to_param
