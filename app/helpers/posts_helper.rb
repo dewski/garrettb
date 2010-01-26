334d2faf79fc066588708d388f65d0b060fc343d
dewski <xhtmlthis@me.com>
2010-01-25 23:59:54 -0800

    fixed specific markdown issues for the show action

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,13 @@
 module PostsHelper
   def markdown(body)
     cutoff = body.split('\end')
-    content = (cutoff.length > 0 && controller.action_name != "show") ? cutoff[0] : body.gsub('\end', '')
+    
+    if controller.action_name == "show"
+      content = body.gsub('\end', '')
+    else
+      content = (cutoff.length > 0) ? cutoff[0] : body
+    end
+    
     BlueCloth.new(content).to_html.html_safe!
   end
 end
